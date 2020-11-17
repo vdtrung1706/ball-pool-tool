5ca1dfbab3eb2d00517d3b0aac1e251d76406b23
trunpyon <trunpyon.it@gmail.com>
2020-11-17 08:00:30 +0700

    add trickshots feature, fanstatic version 1.1

--- pool-tool/Class/Helper.cs
+++ pool-tool/Class/Helper.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace pool_tool.Class {
     public class Helper {
-        public static Point pointsIntersection(Point a, Point b, Point c, Point d) {
+        public static Point intersectionPos(Point a, Point b, Point c, Point d) {
             // Line AB represented as a1x + b1y = c1  
             double a1 = b.Y - a.Y;
             double b1 = a.X - b.X;
@@ -30,6 +33,132 @@ namespace pool_tool.Class {
                 double y = (a1 * c2 - a2 * c1) / determinant;
                 return new Point(x, y);
             }
+        }
+
+        public static Point nextPos(Point firstPos, Point secondPos, Canvas table) {
+
+            var nextPos = new Point(0, 0);
+
+            var corTopLeftPos = new Point(0, 0);
+            var corTopRightPos = new Point(table.ActualWidth, 0);
+            var corBotLeftPos = new Point(0, table.ActualHeight);
+            var corBotRightPos = new Point(table.ActualWidth, table.ActualHeight);
+
+            // table top
+            if (secondPos.X >= 0 && secondPos.X <= table.ActualWidth && secondPos.Y == 0) {
+                var crossBotPos = intersectionPos(firstPos, secondPos, corBotLeftPos, corBotRightPos);
+                var symPos = new Point(crossBotPos.X - 2 * (crossBotPos.X - secondPos.X), table.ActualHeight);
+
+                if (symPos.X < 0) {
+                    nextPos = intersectionPos(secondPos, symPos, corTopLeftPos, corBotLeftPos);
+                }
+                else if (symPos.X > table.ActualWidth) {
+                    nextPos = intersectionPos(secondPos, symPos, corTopRightPos, corBotRightPos);
+                }
+                else {
+                    nextPos = symPos;
+                }
+            }
+
+            // table left
+            else if (secondPos.Y >= 0 && secondPos.Y <= table.ActualHeight && secondPos.X == 0) {
+                var crossRightPos = intersectionPos(firstPos, secondPos, corTopRightPos, corBotRightPos);
+                var symPos = new Point(table.ActualWidth, crossRightPos.Y - 2 * (crossRightPos.Y - secondPos.Y));
+                if (symPos.Y < 0) {
+                    nextPos = intersectionPos(secondPos, symPos, corTopLeftPos, corTopRightPos);
+                }
+                else if (symPos.Y > table.ActualHeight) {
+                    nextPos = intersectionPos(secondPos, symPos, corBotLeftPos, corBotRightPos);
+                }
+                else {
+                    nextPos = symPos;
+                }
+            }
+
+            // table bottom
+            else if (secondPos.X >= 0 && secondPos.X <= table.ActualWidth && secondPos.Y == table.ActualHeight) {
+                var crossTopPos = intersectionPos(firstPos, secondPos, corTopLeftPos, corTopRightPos);
+                var symPos = new Point(crossTopPos.X - 2 * (crossTopPos.X - secondPos.X), 0);
+                if (symPos.X < 0) {
+                    nextPos = intersectionPos(secondPos, symPos, corTopLeftPos, corBotLeftPos);
+                }
+                else if (symPos.X > table.ActualWidth) {
+                    nextPos = intersectionPos(secondPos, symPos, corTopRightPos, corBotRightPos);
+                }
+                else {
+                    nextPos = symPos;
+                }
+            }
+
+            // table right
+            else {
+                var crossLeftPos = intersectionPos(firstPos, secondPos, corTopLeftPos, corBotLeftPos);
+                var symPos = new Point(0, crossLeftPos.Y - 2 * (crossLeftPos.Y - secondPos.Y));
+                if (symPos.Y < 0) {
+                    nextPos = intersectionPos(secondPos, symPos, corTopLeftPos, corTopRightPos);
+                }
+                else if (symPos.Y > table.ActualHeight) {
+                    nextPos = intersectionPos(secondPos, symPos, corBotLeftPos, corBotRightPos);
+                }
+                else {
+                    nextPos = symPos;
+                }
+            }
+            return nextPos;
+        }
+
+        public static void endableEllipse(Ellipse ellipse) {
+            ellipse.Visibility = Visibility.Visible;
+            ellipse.IsEnabled = true;
+        }
+
+        public static void disableEllipse(Ellipse ellipse) {
+            ellipse.Visibility = Visibility.Hidden;
+            ellipse.IsEnabled = false;
+        }
+
+        public static void cleanCanvasChild(Canvas canvas, int index, int count) => canvas.Children.RemoveRange(index, count);
+
+        public static Point getEllipsePos(Ellipse ellipse, Canvas canvas) {
+            var pos = ellipse.TransformToAncestor(canvas).Transform(new Point(0, 0));
+            return new Point(pos.X + ellipse.Height / 2, pos.Y + ellipse.Width / 2);
+        }
+
+        public static void resetLinePos(Line line) => line.X1 = line.X2 = line.Y1 = line.Y2 = 0;
+
+        public static void disablePath(Path path) {
+            path.IsEnabled = false;
+            path.Visibility = Visibility.Hidden;
+            path.Data = Geometry.Parse("");
+        }
+
+        public static void enablePath(Path path, List<Point> listPos = null) {
+            path.IsEnabled = true;
+            path.Visibility = Visibility.Visible;
+            if (listPos != null) {
+                var pathData = "M";
+                foreach (var pos in listPos) {
+                    pathData += " " + pos.ToString();
+                }
+                path.Data = Geometry.Parse(pathData);
+            }
+        }
+
+        public static Point roundPos(Point pos, int digits) {
+            pos.X = Math.Round(pos.X, digits);
+            pos.Y = Math.Round(pos.Y, digits);
+            return pos;
+        }
+
+        public static List<Point> getListPos(List<Point> listPos, Point firstPos, Point secondPos, int lineCount, Canvas canvas) {
+            for (int i = 0; i < lineCount; i++) {
+                var nextPos = Helper.nextPos(firstPos, secondPos, canvas);
+                nextPos = roundPos(nextPos, 8);
+                listPos.Add(nextPos);
+                firstPos = secondPos;
+                secondPos = nextPos;
+            }
+            return listPos;
         }
     }
 }
--- pool-tool/MainWindow.xaml.cs
+++ pool-tool/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.Windows.Media.Animation;
 using Microsoft.Win32;
 using System.Windows.Controls.Primitives;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using MaterialDesignThemes.Wpf;
 
 namespace pool_tool {
     /// <summary>
@@ -32,57 +33,71 @@ namespace pool_tool {
 
         private TableConfig tableConfig = new TableConfig();
 
-        private string btnCurrentPressed = "";
+        private string btnCurPressed = "";
 
-        private void updateDirectLine(string btnName) {
-            cvtable.Children.RemoveRange(4, cvtable.Children.Count - 4);
+        private void updateDirectLine() {
+            // Remove some lines add when using draw line
+            Helper.cleanCanvasChild(cvTable, 6, cvTable.Children.Count - 6);
 
-            if (btnName == "" || ellipse1.Visibility == Visibility.Hidden) {
+            if (btnCurPressed == "" || !ellipse1.IsEnabled || !ellipse2.IsEnabled || btnCurPressed == btnLongLine.Name) {
                 return;
             }
 
-            var pointE1 = ellipse1.TransformToAncestor(cvtable).Transform(new Point(0, 0));
-
-            var pointE2 = ellipse2.TransformToAncestor(cvtable).Transform(new Point(0, 0));
-
-            pointE1 = new Point(pointE1.X + ellipse1.Height / 2, pointE1.Y + ellipse1.Height / 2);
-            pointE2 = new Point(pointE2.X + ellipse2.Height / 2, pointE2.Y + ellipse2.Height / 2);
-
-            var crossPoint = new Point(0, 0);
-
-            line1.X1 = pointE1.X;
-            line1.Y1 = pointE1.Y;
-
-            line2.X2 = pointE2.X;
-            line2.Y2 = pointE2.Y;
+            var e1Pos = Helper.getEllipsePos(ellipse1, cvTable);
+            var e2Pos = Helper.getEllipsePos(ellipse2, cvTable);
 
 
-            if (btnName == btnDownArrow.Name) {
-                crossPoint = Helper.pointsIntersection(pointE1, new Point(pointE2.X, cvtable.ActualHeight),
-                    pointE2, new Point(pointE1.X, cvtable.ActualHeight));
-                line1.Y2 = line2.Y1 = cvtable.ActualHeight;
-                line1.X2 = line2.X1 = crossPoint.X;
+            line1.X1 = e1Pos.X;
+            line1.Y1 = e1Pos.Y;
+
+            line2.X2 = e2Pos.X;
+            line2.Y2 = e2Pos.Y;
+
+            Point crossPos;
+            if (btnCurPressed == btnDownArrow.Name) {
+                crossPos = Helper.intersectionPos(e1Pos, new Point(e2Pos.X, cvTable.ActualHeight),
+                                                  e2Pos, new Point(e1Pos.X, cvTable.ActualHeight));
+                line1.Y2 = line2.Y1 = cvTable.ActualHeight;
+                line1.X2 = line2.X1 = crossPos.X;
             }
-            else if (btnName == btnUpArrow.Name) {
-                crossPoint = Helper.pointsIntersection(pointE1, new Point(pointE2.X, 0),
-                                   pointE2, new Point(pointE1.X, 0));
+            else if (btnCurPressed == btnUpArrow.Name) {
+                crossPos = Helper.intersectionPos(e1Pos, new Point(e2Pos.X, 0),
+                                                  e2Pos, new Point(e1Pos.X, 0));
                 line1.Y2 = line2.Y1 = 0;
-                line1.X2 = line2.X1 = crossPoint.X;
+                line1.X2 = line2.X1 = crossPos.X;
             }
-            else if (btnName == btnLeftArrow.Name) {
-                crossPoint = Helper.pointsIntersection(pointE1, new Point(0, pointE2.Y),
-                                   pointE2, new Point(0, pointE1.Y));
+            else if (btnCurPressed == btnLeftArrow.Name) {
+                crossPos = Helper.intersectionPos(e1Pos, new Point(0, e2Pos.Y),
+                                                  e2Pos, new Point(0, e1Pos.Y));
                 line1.X2 = line2.X1 = 0;
-                line1.Y2 = line2.Y1 = crossPoint.Y;
+                line1.Y2 = line2.Y1 = crossPos.Y;
             }
-            else if (btnName == btnRightArrow.Name) {
-                crossPoint = Helper.pointsIntersection(pointE1, new Point(cvtable.ActualWidth, pointE2.Y),
-                                   pointE2, new Point(cvtable.ActualWidth, pointE1.Y));
-                line1.X2 = line2.X1 = cvtable.ActualWidth;
-                line1.Y2 = line2.Y1 = crossPoint.Y;
+            else if (btnCurPressed == btnRightArrow.Name) {
+                crossPos = Helper.intersectionPos(e1Pos, new Point(cvTable.ActualWidth, e2Pos.Y),
+                                                  e2Pos, new Point(cvTable.ActualWidth, e1Pos.Y));
+                line1.X2 = line2.X1 = cvTable.ActualWidth;
+                line1.Y2 = line2.Y1 = crossPos.Y;
             }
 
         }
+
+
+        private void writeSettingFile(Point menuPos, string message = "") {
+            using (StreamWriter writer = new StreamWriter("./Data/table-config.txt")) {
+                writer.WriteLine($"height_{tableConfig.height}");
+                writer.WriteLine($"width_{tableConfig.width}");
+                writer.WriteLine($"top_{tableConfig.top}");
+                writer.WriteLine($"left_{tableConfig.left}");
+                writer.WriteLine($"right_{tableConfig.right}");
+                writer.WriteLine($"ball_{tableConfig.ballSize}");
+                writer.WriteLine($"menuTop_{menuPos.Y}");
+                writer.WriteLine($"menuLeft_{menuPos.X}");
+                if (message.Length > 0) {
+                    MessageBox.Show("Saved!", "Table Config", MessageBoxButton.OK);
+                }
+            }
+        }
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
             #region READ FILE SETTING
@@ -93,32 +108,34 @@ namespace pool_tool {
                 double value = 0;
                 double.TryParse(strValue[1], out value);
 
-                if (strValue[0] == "height") {
-                    tableConfig.height = value;
-                }
-                else if (strValue[0] == "width") {
-                    tableConfig.width = value;
-                }
-                else if (strValue[0] == "top") {
-                    tableConfig.top = value;
-                }
-                else if (strValue[0] == "left") {
-                    tableConfig.left = value;
-                }
-                else if (strValue[0] == "right") {
-                    tableConfig.right = value;
-                }
-                else if (strValue[0] == "bottom") {
-                    tableConfig.bottom = value;
-                }
-                else if (strValue[0] == "ball") {
-                    tableConfig.ballSize = value;
-                }
-                else if (strValue[0] == "menuTop") {
-                    tableConfig.menuTop = value;
-                }
-                else {
-                    tableConfig.menuLeft = value;
+                switch (strValue[0]) {
+                    case "height":
+                        tableConfig.height = value;
+                        break;
+                    case "width":
+                        tableConfig.width = value;
+                        break;
+                    case "top":
+                        tableConfig.top = value;
+                        break;
+                    case "left":
+                        tableConfig.left = value;
+                        break;
+                    case "right":
+                        tableConfig.right = value;
+                        break;
+                    case "bottom":
+                        tableConfig.bottom = value;
+                        break;
+                    case "ball":
+                        tableConfig.ballSize = value;
+                        break;
+                    case "menuTop":
+                        tableConfig.menuTop = value;
+                        break;
+                    default:
+                        tableConfig.menuLeft = value;
+                        break;
                 }
             }
             brdTable.DataContext = tableConfig;
@@ -131,7 +148,6 @@ namespace pool_tool {
         }
 
         #region MENU
-
 
         private void SettingScreen_SizeChange(TableConfig newTable) {
             tableConfig = newTable;
@@ -162,6 +178,7 @@ namespace pool_tool {
 
             var menuPos = brdMenu.TransformToAncestor(cvBody).Transform(new Point(0, 0));
 
+
             if (settingScreen.ShowDialog() == true) {
                 writeSettingFile(menuPos, "Saved");
             }
@@ -170,30 +187,15 @@ namespace pool_tool {
                 brdTable.Height = tableConfig.height;
                 brdTable.Width = tableConfig.width;
                 brdTable.Margin = new Thickness(tableConfig.left, tableConfig.top, tableConfig.right, tableConfig.bottom);
-                brdTable.BorderThickness = new Thickness(0);
             }
+            brdTable.BorderThickness = new Thickness(0);
         }
 
-        private void writeSettingFile(Point menuPos, string message = "") {
-            using (StreamWriter writer = new StreamWriter("./Data/table-config.txt")) {
-                writer.WriteLine($"height_{tableConfig.height}");
-                writer.WriteLine($"width_{tableConfig.width}");
-                writer.WriteLine($"top_{tableConfig.top}");
-                writer.WriteLine($"left_{tableConfig.left}");
-                writer.WriteLine($"right_{tableConfig.right}");
-                writer.WriteLine($"ball_{tableConfig.ballSize}");
-                writer.WriteLine($"menuTop_{menuPos.Y}");
-                writer.WriteLine($"menuLeft_{menuPos.X}");
-                if (message.Length > 0) {
-                    MessageBox.Show("Saved!", "Table Config", MessageBoxButton.OK);
-                }
-            }
-        }
 
         private void btnBalls_Click(object sender, RoutedEventArgs e) {
-            if (ellipse1.Visibility == Visibility.Hidden) {
-                ellipse1.Visibility = ellipse2.Visibility = Visibility.Visible;
-                ellipse1.IsEnabled = ellipse2.IsEnabled = true;
+            if (!ellipse1.IsEnabled && !ellipse2.IsEnabled) {
+                Helper.endableEllipse(ellipse1);
+                Helper.endableEllipse(ellipse2);
 
                 // set position of two balls
                 Canvas.SetLeft(ellipse1, (brdTable.ActualWidth / 2 - ellipse1.ActualWidth));
@@ -203,55 +205,60 @@ namespace pool_tool {
                 Canvas.SetTop(ellipse2, brdTable.ActualHeight / 2 - ellipse2.ActualHeight / 2);
             }
             else {
-                ellipse1.Visibility = ellipse2.Visibility = Visibility.Hidden;
-                ellipse1.IsEnabled = ellipse2.IsEnabled = false;
+                Helper.disableEllipse(ellipse1);
+                Helper.disableEllipse(ellipse2);
             }
-            btnCurrentPressed = "";
-            reDefaultLines();
-            cvtable.Children.RemoveRange(4, cvtable.Children.Count - 4);
+            Helper.resetLinePos(line1);
+            Helper.resetLinePos(line2);
+            Helper.cleanCanvasChild(cvTable, 6, cvTable.Children.Count - 6);
+            btnCurPressed = "";
         }
 
         private void btnDownArrow_Click(object sender, RoutedEventArgs e) {
-            btnCurrentPressed = btnDownArrow.Name;
-            reDefaultLines();
-            updateDirectLine(btnCurrentPressed);
+            btnCurPressed = btnDownArrow.Name;
+            Helper.resetLinePos(line1);
+            Helper.resetLinePos(line2);
+            updateDirectLine();
         }
 
         // Clear
         private void btnClear_Click(object sender, RoutedEventArgs e) {
-            ellipse1.Visibility = ellipse2.Visibility = Visibility.Hidden;
-            ellipse1.IsEnabled = ellipse2.IsEnabled = false;
+            Helper.disableEllipse(ellipse1);
+            Helper.disableEllipse(ellipse2);
 
-            reDefaultLines();
+            Helper.resetLinePos(line1);
+            Helper.resetLinePos(line2);
 
-            btnCurrentPressed = "";
-            cvtable.Children.RemoveRange(4, cvtable.Children.Count - 4);
+            Helper.cleanCanvasChild(cvTable, 6, cvTable.Children.Count - 6);
+            Helper.disablePath(pathLines);
+            Helper.disableEllipse(ellipseMove);
+            btnCurPressed = "";
         }
 
-        private void reDefaultLines() {
-            line1.X1 = line1.Y1 = line1.X2 = line1.Y2 = line2.X2 = line2.Y2 = line2.X1 = line2.Y1 = 0;
-        }
 
         private void btnUpArrow_Click(object sender, RoutedEventArgs e) {
-            btnCurrentPressed = btnUpArrow.Name;
-            reDefaultLines();
-            updateDirectLine(btnCurrentPressed);
+            btnCurPressed = btnUpArrow.Name;
+            Helper.resetLinePos(line1);
+            Helper.resetLinePos(line2);
+            updateDirectLine();
         }
 
         private void btnRightArrow_Click(object sender, RoutedEventArgs e) {
-            btnCurrentPressed = btnRightArrow.Name;
-            reDefaultLines();
-            updateDirectLine(btnCurrentPressed);
+            btnCurPressed = btnRightArrow.Name;
+            Helper.resetLinePos(line1);
+            Helper.resetLinePos(line2);
+            updateDirectLine();
         }
 
         private void btnLeftArrow_Click(object sender, RoutedEventArgs e) {
-            btnCurrentPressed = btnLeftArrow.Name;
-            reDefaultLines();
-            updateDirectLine(btnCurrentPressed);
+            btnCurPressed = btnLeftArrow.Name;
+            Helper.resetLinePos(line1);
+            Helper.resetLinePos(line2);
+            updateDirectLine();
         }
         private void btnLine_Click(object sender, RoutedEventArgs e) {
             btnClear_Click(sender, e);
-            btnCurrentPressed = btnLine.Name;
+            btnCurPressed = oneLine.Name;
         }
 
         #endregion
@@ -285,42 +292,42 @@ namespace pool_tool {
         }
 
         private void MoveEllipse(MouseEventArgs e, Ellipse ellipse) {
-            var pos = e.GetPosition(cvtable);
+            var pos = e.GetPosition(cvTable);
             Canvas.SetLeft(ellipse, pos.X - ellipse.Width / 2);
             Canvas.SetTop(ellipse, pos.Y - ellipse.Height / 2);
-            updateDirectLine(btnCurrentPressed);
+            updateDirectLine();
         }
         #endregion
 
 
         private void brdTable_PreviewMouseMove(object sender, MouseEventArgs e) {
-            if (e.LeftButton == MouseButtonState.Pressed && btnCurrentPressed == btnLine.Name) {
-                var curPoint = e.GetPosition(cvtable);
-                line1.X2 = curPoint.X;
-                line1.Y2 = curPoint.Y;
+            if (e.LeftButton == MouseButtonState.Pressed && btnCurPressed == oneLine.Name) {
+                var curPos = e.GetPosition(cvTable);
+                line1.X2 = curPos.X;
+                line1.Y2 = curPos.Y;
             }
         }
 
         private void brdTable_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-            if (e.LeftButton == MouseButtonState.Pressed && btnCurrentPressed == btnLine.Name) {
-                var curPoint = e.GetPosition(cvtable);
-                line1.X1 = curPoint.X;
-                line1.Y1 = curPoint.Y;
+            if (e.LeftButton == MouseButtonState.Pressed && btnCurPressed == oneLine.Name) {
+                var curPos = e.GetPosition(cvTable);
+                line1.X1 = curPos.X;
+                line1.Y1 = curPos.Y;
             }
         }
 
         private void brdTable_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
-            if (btnCurrentPressed == btnLine.Name) {
+            if (btnCurPressed == oneLine.Name) {
                 var line = new Line() {
                     Stroke = new SolidColorBrush(Colors.Black),
-                    Opacity = 0.5,
-                    StrokeThickness = 5,
+                    Opacity = 0.7,
+                    StrokeThickness = 2,
                     X1 = line1.X1,
                     Y1 = line1.Y1,
                     X2 = line1.X2,
                     Y2 = line1.Y2
                 };
-                cvtable.Children.Add(line);
+                cvTable.Children.Add(line);
             }
         }
 
@@ -340,11 +347,76 @@ namespace pool_tool {
 
         private void btnMove_PreviewMouseMove(object sender, MouseEventArgs e) {
             var pos = Mouse.GetPosition(cvBody);
-            curPoint.Text = $"{pos.ToString()}";
             if (btnMove.IsMouseCaptured) {
                 Canvas.SetLeft(brdMenu, pos.X - btnMove.Width / 2);
                 Canvas.SetTop(brdMenu, pos.Y - btnMove.Width / 2);
             }
+        }
+
+        private void ellipseMove_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                ellipseMove.CaptureMouse();
+            }
+        }
+
+        private void ellipseMove_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            if (ellipseMove.IsMouseCaptured) {
+                ellipseMove.ReleaseMouseCapture();
+            }
+        }
+
+        private void ellipseMove_MouseMove(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed && btnCurPressed == btnLongLine.Name) {
+
+                var mousePos = Mouse.GetPosition(cvTable);
+
+                Canvas.SetLeft(ellipseMove, mousePos.X - 5);
+                Canvas.SetTop(ellipseMove, mousePos.Y - 5);
+
+                var ballPos = Helper.getEllipsePos(ellipse1, cvTable);
+
+                CurPoint.Text = mousePos.ToString();
+
+                var listPos = new List<Point>();
+                listPos.Add(ballPos);
+
+                var firstPos = ballPos;
+                if (mousePos.X > 0 && mousePos.Y < 0 && mousePos.X < cvTable.ActualWidth) {
+                    var secondPos = new Point(mousePos.X, 0);
+                    listPos.Add(secondPos);
+                    listPos = Helper.getListPos(listPos, firstPos, secondPos, 4, cvTable);
+
+                }
+
+                // table left
+                else if (mousePos.X < 0 && mousePos.Y > 0 && mousePos.Y < cvTable.ActualHeight) {
+                    var secondPos = new Point(0, mousePos.Y);
+                    listPos.Add(secondPos);
+                    listPos = Helper.getListPos(listPos, firstPos, secondPos, 4, cvTable);
+                }
+
+                // table bottom
+                else if (mousePos.X > 0 && mousePos.Y > cvTable.ActualHeight && mousePos.X < cvTable.ActualWidth) {
+                    var secondPos = new Point(mousePos.X, cvTable.ActualHeight);
+                    listPos.Add(secondPos);
+                    listPos = Helper.getListPos(listPos, firstPos, secondPos, 4, cvTable);
+                }
+
+                // table right
+                else if (mousePos.X > cvTable.ActualWidth && mousePos.Y <= cvTable.ActualHeight && mousePos.Y > 0) {
+                    var secondPos = new Point(cvTable.ActualWidth, mousePos.Y);
+                    listPos.Add(secondPos);
+                    listPos = Helper.getListPos(listPos, firstPos, secondPos, 4, cvTable);
+                }
+                Helper.enablePath(pathLines, listPos);
+            }
+        }
+
+        private void btnLongLine_Click(object sender, RoutedEventArgs e) {
+            btnClear_Click(sender, e);
+            btnCurPressed = btnLongLine.Name;
+            Helper.endableEllipse(ellipse1);
+            Helper.endableEllipse(ellipseMove);
         }
     }
 
